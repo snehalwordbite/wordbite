99153aa9ef78a1c98c241058eb69957a55b0ad0c
ankitkumardubey-dev <ankitdubey.in@wordbite.com>
2021-05-13 12:00:26 +0530

    created components for profile page

--- profile.php
+++ profile.php
@@ -1,6 +1,7 @@
 <?php session_start(); ?>
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <title><?php
@@ -9,22 +10,33 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/png" href="./assets/img/logo.png">
-    <!-- bootstrap css file -->
-    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  <!-- bootstrap css file -->
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
 </head>
+
 <body>
-    
-   <div class="container-fluid">
-   <?php
+
+  <div class="container-fluid">
+    <?php
     require('./components/header.php');
-    require('./components/profileSidebar.php');
     ?>
 
-   </div>
+    <div class="row flex-xl-nowrap">
+  <?php
+    require('./components/profileSidebar.php');
+  ?>
+      <main class="col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content" role="main">
+        this is the main part of the profile page
+      </main>
+    </div>
 
 
-<script src="./assets/js/jquery-3.5.1.slim.min.js"></script>
-<script src="./assets/js/popper.min.js"></script>
-<script src="./assets/js/bootstrap.min.js"></script>
+  </div>
+
+
+  <script src="./assets/js/jquery-3.5.1.slim.min.js"></script>
+  <script src="./assets/js/popper.min.js"></script>
+  <script src="./assets/js/bootstrap.min.js"></script>
 </body>
+
 </html>
